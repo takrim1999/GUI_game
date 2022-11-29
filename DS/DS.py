b23f0ff1f9b945c6b36607d2e0630ba0e4d4afa3
takrim1999 <takrimulalom@gmail.com>
2022-11-30 02:06:44 +0600

    Better Version of Stack

--- DS/DS.py
+++ DS/DS.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
 class stack:
-    def __init__(self,elements):
-        self.elements = elements    
+    def __init__(self):
+        self.elements = ""
+        self.length = 0     
+    
+    def is_empty(self):
+        if self.length == 0:
+            return True
+        else:
+            return False
+    
     def push(self,new_element):
+        if self.is_empty() == False:
+            self.elements += ","
+            self.length += 1
         self.elements += new_element
+        self.length += 1
         return self.elements
+    
     def pop(self):
-        self.elements = self.elements[:self.__len__()-1]
+        self.elements = self.elements[:self.__len__()-2]
+    
     def __len__(self):
-        count = 0
-        for i in self.elements:
-            count+=1
-        return count
+        return self.length
+    
     def __str__(self):
         return self.elements
-
-new = stack("12345")
-print(new)
-new.pop()
-print(new)
