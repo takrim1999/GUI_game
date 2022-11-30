38df5f1e916fd687cf002497b2dfa99c3f23424d
takrim1999 <takrimulalom@gmail.com>
2022-12-01 00:44:08 +0600

    Multiple Turtles are ok. but not working any other methods.

--- TurtleRace/game.py
+++ TurtleRace/game.py
@@ -2,28 +2,36 @@
 import turtle
 import random
 
-class Turtles(turtle.Turtle):
+class Turtles:
     def __init__(self,name="Give name",color="red",x=0,y=0,shape="turtle"):
         self.name = name
         self.color = color
         self.shape = shape
         self.x = x
         self.y = y
-        turtle.shape(self.shape)
-        turtle.color(self.color)
-        turtle.setpos(self.x,self.y)
-
+    def initialize(self):
+        new = turtle.Turtle()
+        new.penup()
+        new.shape(self.shape)
+        new.color(self.color)
+        new.setpos(self.x,self.y)
+    def run(self):
+        # new = self.new
+        for i in range(100):
+            new.forward(1)
         
     def __str__(self):
         return "My name is {}".format(self.name)
 
 def main():
     turtle.Screen()
-    turtle.penup()
     red = Turtles(name="Red Turtle",color="red",x=-200,y=0)
     blue = Turtles(name="Blue Turtle",color="blue",x=-200,y=-200)
     green = Turtles(name="Green Turtle",color="green",x=-200,y=200)
+    red.initialize()
+    red.run()
+    
+    
     turtle.done()
 
-# print(red)
 main()
