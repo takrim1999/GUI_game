d0d2bfa775feb09f5be14e4be7399499b62bbbfd
takrim1999 <takrimulalom@gmail.com>
2022-12-02 15:35:05 +0600

    Finish Line Done

--- TurtleRace/game.py
+++ TurtleRace/game.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
 import turtle
 import random
+turtle.Screen().setup(width=0.75, height=0.8, startx=None, starty=None)
+turtle.screensize(bg="green")
+def finish_line(x=480,y=300,width=15,line_pair=10,line_length=30):
+    line = turtle.Turtle()
+    line.speed(0)
+    line.width(width)
+    line.penup()
+    line.setpos(x,y)
+    line.pendown()
+    line.setheading(270)
+    for i in range(line_pair):
+        line.color("black")
+        line.forward(line_length)
+        line.color("white")
+        line.forward(line_length)
+    line.penup()
+    line.setpos(x+width,y-line_pair*2*line_length)
+    line.setheading(90)
+    line.pendown()
+    for i in range(line_pair):
+        line.color("black")
+        line.forward(line_length)
+        line.color("white")
+        line.forward(line_length)   
+# def turtles():
+#     for i in range
 
-class Turtles:
-    def __init__(self,name="Give name",color="red",x=0,y=0,shape="turtle"):
-        self.name = name
-        self.color = color
-        self.shape = shape
-        self.x = x
-        self.y = y
-    def initialize(self):
-        new = turtle.Turtle()
-        new.penup()
-        new.shape(self.shape)
-        new.color(self.color)
-        new.setpos(self.x,self.y)
-    def run(self):
-        # new = self.new
-        for i in range(100):
-            new.forward(1)
-        
-    def __str__(self):
-        return "My name is {}".format(self.name)
 
-def main():
-    turtle.Screen()
-    red = Turtles(name="Red Turtle",color="red",x=-200,y=0)
-    blue = Turtles(name="Blue Turtle",color="blue",x=-200,y=-200)
-    green = Turtles(name="Green Turtle",color="green",x=-200,y=200)
-    red.initialize()
-    red.run()
-    
-    
-    turtle.done()
+finish_line()
 
-main()
+turtle.done()
